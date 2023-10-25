ad2e098359ea104aa41642ae87429394407db181
chikko80 <seitzmanuel@t-online.de>
2023-10-25 18:29:51 +0200

    test

--- setup.sh
+++ setup.sh
@@ -30,12 +30,12 @@ function main() {
 
     # # Run setup functions
 
-    # read -rp "Enter the username of the new user account: " username
-    # read -rsp "Enter the password for the new user account: " password
-    # read -rp "Enter the email for SSL certificates: " traefik_email
-    # read -rp "Enter the domain for Traefik basis domain (used for webgui): " traefik_domain
-    # read -rp "Enter the username for the Traefik web GUI: " webgui_user
-    # read -rsp "Enter the password for the Traefik web GUI: " webgui_pass
+    read -rp "Enter the username of the new user account: " username
+    read -rsp "Enter the password for the new user account: " password
+    read -rp "Enter the email for SSL certificates: " traefik_email
+    read -rp "Enter the domain for Traefik basis domain (used for webgui): " traefik_domain
+    read -rp "Enter the username for the Traefik web GUI: " webgui_user
+    read -rsp "Enter the password for the Traefik web GUI: " webgui_pass
 
 
     # echo  # This is to move to the next line after the silent password prompt
